c75213c2680ab04ef9a563dc195ef0ed8abcec9d
Osamah Gharad <osamah.gharred@gmail.com>
2020-08-21 00:55:00 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,4 +2,5 @@ const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
 // Code your solution here
 totalBatteries =()=>{
-  return batteryBatches.reduce((batteryBatch,totalBattery) =>  batteryBatch+totalBattery,0)}
+  return batteryBatches.reduce((batteryBatch,totalBattery) =>  batteryBatch+totalBattery,10)}
+console.log(totalBatteries())
